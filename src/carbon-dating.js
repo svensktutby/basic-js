80f2c4b25b1fef0f09cde9820f0802b4510de50c
svensktutby <svensk.tut.by@gmail.com>
2020-02-20 20:45:19 +0300

    feat: solve Carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  sampleActivity = parseFloat(sampleActivity);
+
+  return typeof !isNaN(sampleActivity) &&
+    sampleActivity > 0 &&
+    sampleActivity < MODERN_ACTIVITY ?
+    Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)) :
+    false;
 };

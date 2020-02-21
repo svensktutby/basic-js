34eeea466fa05216e2a6f5dee83f78c0d73c7444
svensktutby <svensk.tut.by@gmail.com>
2020-02-21 14:27:57 +0300

    feat: solve What season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,22 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Not [object Date]');
+  }
+
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  const month = date.getMonth();
+
+  if (month >= 0 && month < 2 || month === 11) {
+    return seasons[0];
+  } else if (month > 1 && month < 5) {
+    return seasons[1];
+  } else if (month > 4 && month < 8) {
+    return seasons[2];
+  } else if (month > 7 && month < 11) {
+    return seasons[3];
+  }
 };

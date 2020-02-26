dfb72de52870ac64e4a75ad6bdd7a8ca192c7505
svensktutby <svensk.tut.by@gmail.com>
2020-02-26 22:40:51 +0300

    feat: solve Tower of Hanoi task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,12 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = 1;
+
+  for (let i = 1; i < disksNumber; i++) {
+    result = result * 2 + 1;
+  }
+  
+  return {
+    turns: result,
+    seconds: result / (turnsSpeed / (60 * 60))
+  };
+};

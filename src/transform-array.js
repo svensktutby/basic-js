3a3793079de6dec9f724a68f2f6ae6096aec0ff6
svensktutby <svensk.tut.by@gmail.com>
2020-02-27 11:35:33 +0300

    feat: solve Transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('"arr" in not an Array');
+  }
+
+  if (!arr.length) {
+    return arr;
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    switch (arr[i]) {
+      case '--double-next':
+        (i < arr.length - 1) ? newArr.push(arr[i + 1]) : i;
+        break;
+      case '--double-prev':
+        (i > 0) ? newArr.push(arr[i - 1]) : i;
+        break;
+      case '--discard-next':
+        (i < arr.length - 1) ? i++ : i;
+        break;
+      case '--discard-prev':
+        (newArr.length > 0) ? newArr.pop() : i;
+        break;
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
+  }
+
+  return newArr;
 };

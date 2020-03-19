a3d7d1f59a83adbf583af5671716d5b46d4e9a7d
svensktutby <svensk.tut.by@gmail.com>
2020-03-19 12:27:15 +0300

    feat: solve Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  let result = '';
+  let add = '';
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    (i === additionRepeatTimes - 1) ?
+    add += addition :
+    add += addition + additionSeparator;
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    (i === repeatTimes - 1) ?
+    result += str + add :
+    result += str + add + separator;
+  }
+
+  return result;
 };
-  

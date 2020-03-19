7a2cf2717debe082eef522b3e16ba096329d2560
svensktutby <svensk.tut.by@gmail.com>
2020-03-19 23:15:58 +0300

    feat: solve Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,83 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.startASCII = 65;
+    this.endASCII = 90;
+  }
+  
+
+  getEncryptNumber(num1, num2) {
+    let maxNumber = 26;
+    let result = num1 + num2;
+
+    return result >= maxNumber ? (result - maxNumber) : result;
+  }
+
+  getDecryptNumber(num1, num2) {
+    let maxNumber = 26;
+    let minNumber = 0;
+    let result = num1 - num2;
+
+    return result < minNumber ? (result + maxNumber) : result;
+  }
+
+  getVigenereLetter(num) {
+    return String.fromCharCode(num + this.startASCII);
+  }
+
+  getSameLengthKeyword(str, keyword) {
+    let result = '';
+    while(result.length < str.length) {
+      result += keyword;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    return result.slice(0, str.length);
+  }
+
+  getVigenereStr(str, keyword, vigenereFun) {
+    let index = 0;
+    const sameLengthKeyword = this.getSameLengthKeyword(str, keyword);
+
+    const charArr = str.split('').map((item) => {
+      const char = item.toUpperCase();
+      const keyLetter = sameLengthKeyword[index].toUpperCase();
+
+      if (char.charCodeAt(0) >= this.startASCII && char.charCodeAt(0) <= this.endASCII) {
+        index++;
+        const num1 = char.charCodeAt(0) - this.startASCII;
+        const num2 = keyLetter.charCodeAt(0) - this.startASCII;
+        
+        return this.getVigenereLetter(vigenereFun(num1, num2));
+      } else {
+        return char;
+      }
+    });
+
+    return this.type ?
+      charArr.join('') :
+      charArr.reverse().join('');
+  }
+
+  encrypt(str, keyword) {
+    if (!str || !keyword) {
+      throw new Error('Both parameters must be passed!');
     }
+
+    const encrypted = this.getVigenereStr(str, keyword, this.getEncryptNumber);
+
+    return encrypted;
+
+  }
+
+  decrypt(str, keyword) {
+    if (!str || !keyword) {
+      throw new Error('Both parameters must be passed!');
+    }
+
+    const decrypted = this.getVigenereStr(str, keyword, this.getDecryptNumber);
+
+    return decrypted;
+  }
 }
 
 module.exports = VigenereCipheringMachine;
